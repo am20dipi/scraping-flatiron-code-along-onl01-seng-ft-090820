8300057c1e0955b35f06a407f8c5e80b102280ee
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-28 18:22:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,15 +18,10 @@ class Scraper
   end
   
   def get_page
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.githumb.io/site-for-scraping/courses"))
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     binding.pry
   end 
-  
-  def get_courses
-  end
-  
-  def make_courses
-  end
+
   
 end
 
